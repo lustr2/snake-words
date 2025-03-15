c166fac0da1c36d3637c199a381b7007f400b476
Lucie Struplova <lustruplova@gmail.com>
2025-03-15 18:12:16 +0100

    Upravy configurace pro zverejneni a debug na locale

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
